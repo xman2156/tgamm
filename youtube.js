cd8bf7c74eeeb1de573e6bf0d5c50ec87eee1c66
xman2156 <34029777+xman2156@users.noreply.github.com>
2021-12-05 19:37:14 -0600

    Bug fixes

--- youtube.js
+++ youtube.js
@@ -11,7 +11,7 @@ youtube.addEventListener( "click", function() {
   var iframe = document.createElement( "iframe" );
 	iframe.setAttribute("frameborder", "0");
 	iframe.setAttribute("allowfullscreen", "");
-	iframe.setAttribute("src", "https://www.youtube.com/RQCn8hg-6zg?autoplay=1");
+	iframe.setAttribute("src", "https://www.youtube.com/embed/RQCn8hg-6zg?autoplay=1");
   this.innerHTML = "";
 	this.appendChild(iframe);
 });
